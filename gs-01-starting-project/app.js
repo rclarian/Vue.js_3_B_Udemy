4a3706872bcc756a9dc006496d4d9677c65da57c
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2025-08-17 13:59:44 +0800

    #17. Creating and connecting Vue App Instances
    #17. Creating and connecting Vue App Instances

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,28 +1,9 @@
-Vue.createApp({
+const app = Vue.createApp({
   data() {
     return {
-      goals: [],
-      enteredValue: "",
+        courseGoal: 'Finish the course and lear Vue!'
     };
   },
-  methods: {
-    addGoal() {
-      this.goals.push(this.enteredValue);
-      this.enteredValue = "";
-    },
-  },
-}).mount("#app");
+});
 
-// const buttonEl = document.querySelector('button');
-// const inputEl = document.querySelector('input');
-// const listEl = document.querySelector('ul');
-
-// function addGoal(){
-//     const enteredValue = inputEl.value;
-//     const listItemEl = document.createElement('li');
-//     listItemEl.textContent = enteredValue;
-//     listEl.appendChild(listItemEl);
-//     inputEl.value = '';
-// }
-
-// buttonEl.addEventListener('click', addGoal);
+app.mount("#user-goal");
