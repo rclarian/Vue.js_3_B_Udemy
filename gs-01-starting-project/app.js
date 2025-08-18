26ec0cbb00071a1cd2446ac775fb740ac6ac02cf
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2025-08-18 11:09:53 +0800

    #20. Understanding "methods" in Vue Apps
    #20. Understanding "methods" in Vue Apps

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,9 +1,19 @@
 const app = Vue.createApp({
   data() {
     return {
-        courseGoal: 'Finish the course and lear Vue!',
-        vueLink: 'https://vuejs.org/'
+      courseGoal: "Finish the course and lear Vue!",
+      vueLink: "https://vuejs.org/",
     };
+  },
+  methods: {
+    outputGoal() {
+      var randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return "Learn Vue!";
+      } else {
+        return "Master Vue!";
+      }
+    },
   },
 });
 
