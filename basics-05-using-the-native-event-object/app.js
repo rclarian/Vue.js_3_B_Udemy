099b5680d897f87e446121bf3930db8a773e3735
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2025-08-21 13:42:00 +0800

    #33. Working with Watchers (watch: {})
    #33. Working with Watchers (watch: {})

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -2,28 +2,55 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      //fullName: '',
     };
+  },
+  watch: {
+    counter(value) {
+      if(value > 50){
+        const that = this;
+        setTimeout(function(){
+          that.counter = 0;
+        },2000);
+      }
+    }
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullName = '';
+    //   } else {
+    //     this.fullName = value + " " + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullName = '';
+    //   } else {
+    //     this.fullName = this.name + " " + value;
+    //   }
+    // },
   },
   computed: {
     fullName(){
       console.log('Running computed again....')
-      if(this.name === ''){
+      if(this.name === '' || this.lastName === ''){
         return '';
       }
-      return this.name + ' ' + 'Clarian';
+      return this.name + ' ' + this.lastName;
     }
   },
   methods: {
-    outFullName(){
-      console.log('Running methods again....')
-      if(this.name === ''){
-        return '';
+    outFullName() {
+      console.log("Running methods again....");
+      if (this.name === "") {
+        return "";
       }
-      return this.name + ' ' + 'Clarian';
+      return this.name + " " + this.lastName;
     },
     resetInput() {
       this.name = '';
+      this.lastName = '';
     },
     setName(event) {
       this.name = event.target.value;
@@ -34,8 +61,8 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
-    }
-  }
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
