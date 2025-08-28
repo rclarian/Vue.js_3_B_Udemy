afa7db90bf8c8523a2aac22e2a99ab85b90fb373
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2025-08-28 11:33:49 +0800

    #74. Introducing Components
    #74. Introducing Components

--- cmp-intro-01-starting-setup/app.js
+++ cmp-intro-01-starting-setup/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         {
           id: "manuel",
@@ -18,10 +17,34 @@ const app = Vue.createApp({
       ],
     };
   },
+});
+
+app.component("friend-contact", {
+  template: `
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails()">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "09478851662",
+        email: "manuel@yahoo.com",
+      },
+    };
+  },
   methods: {
-    toggleDetails(id) {
-        this.detailsAreVisible = !this.detailsAreVisible;
-    }
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
   },
 });
 
