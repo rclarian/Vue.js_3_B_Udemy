e167685ba3042f50f52579c69e6974312edff63c
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2025-08-28 21:26:34 +0800

    #88. Creating a Basic Vue App
    #88. Creating a Basic Vue App

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "09478851662",
+          email: "manuel@yahoo.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09478851663",
+          email: "julie@yahoo.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
