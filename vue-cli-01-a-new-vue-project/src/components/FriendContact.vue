aefef410a798ccb8165dedb160dcccd5592d136d
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2025-08-29 14:03:59 +0800

    #97. Validating Props
    #97. Validating Props

--- vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,8 +1,10 @@
 <template>
   <li>
-    <h2>{{ name }} {{ friendIsFavorite === '1' ? '(Favorite)' : '' }}</h2>
-    <button @click="toggleFavorite"> Toggle Favorite</button>
-    <button @click="toggleDetails"> {{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <h2>{{ name }} {{ friendIsFavorite === "1" ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
     <ul v-if="detailsAreVisible">
       <li><strong>Phone:</strong> {{ phoneNumber }}</li>
       <li><strong>Email:</strong> {{ emailAddress }}</li>
@@ -13,12 +15,34 @@
 <script>
 export default {
   name: "FriendContact",
-  props: [
-    'name',
-    'phoneNumber',
-    'emailAddress',
-    'isFavorite'
-  ],
+  // props: [
+  //   'name',
+  //   'phoneNumber',
+  //   'emailAddress',
+  //   'isFavorite'
+  // ],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: String,
+      required: false,
+      default: "0",
+      validator: function (value) {
+        return value === "1" || value === "0";
+      },
+    },
+  },
   data() {
     return {
       detailsAreVisible: false,
@@ -35,13 +59,13 @@ export default {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
-    toggleFavorite(){
-      if(this.friendIsFavorite === '1'){
-        this.friendIsFavorite = '0';
-      }else{
-        this.friendIsFavorite = '1';
+    toggleFavorite() {
+      if (this.friendIsFavorite === "1") {
+        this.friendIsFavorite = "0";
+      } else {
+        this.friendIsFavorite = "1";
       }
-    }
+    },
   },
 };
 </script>
