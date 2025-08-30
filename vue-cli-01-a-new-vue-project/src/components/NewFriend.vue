5e81f75e8c8abd9b24843df0dede9cc903783095
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2025-08-30 19:16:33 +0800

    #103. Demo: Adding Components & Connecting Them - (using emit)
    #103. Demo: Adding Components & Connecting Them - (using emit)

---
+++ vue-cli-01-a-new-vue-project/src/components/NewFriend.vue
@@ -0,0 +1,43 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label for="name">Name</label>
+      <input id="name" type="text" v-model="enteredName" />
+    </div>
+    <div>
+      <label for="phone">Phone</label>
+      <input id="phone" type="tel" v-model="enteredPhone" />
+    </div>
+    <div>
+      <label for="email">Email</label>
+      <input id="email" type="email" v-model="enteredEmail" />
+    </div>
+    <div>
+      <button>Add Contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "NewFriend",
+  emits: ["add-contact"],
+  data() {
+    return {
+      enteredName: "",
+      enteredPhone: "",
+      enteredEmail: "",
+    };
+  },
+  methods: {
+    submitData() {
+        this.$emit('add-contact', this.enteredName, this.enteredPhone, this.enteredEmail);
+        this.enteredName = '';
+        this.enteredPhone = '';
+        this.enteredEmail = '';
+    },
+  },
+};
+</script>
+
+<style scoped></style>
